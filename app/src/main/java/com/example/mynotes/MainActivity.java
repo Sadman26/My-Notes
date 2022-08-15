8bcb2467c4bab408f0e9ddeff6d49353c29efbac
Sadman-123 <19201103123@cse.bubt.edu.bd>
2022-08-16 04:08:28 +0600

    Transparent Statusbar and no actionbar

--- app/src/main/java/com/example/mynotes/MainActivity.java
+++ app/src/main/java/com/example/mynotes/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.mynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //-----Removed Action bar and Statusbar Transparent--------
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getSupportActionBar().hide();
+        //-----------------------------------------------------------
         setContentView(R.layout.activity_main);
     }
 }
