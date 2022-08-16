97556309b3914e7ac17f3e3b756736a8060b52b2
Sadman-123 <19201103123@cse.bubt.edu.bd>
2022-08-16 20:22:02 +0600

    Edited a little bit login page

--- app/src/main/java/com/example/mynotes/Login.java
+++ app/src/main/java/com/example/mynotes/Login.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
-
+    private EditText signupemail,signuppassword;
+    private Button signupbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +24,37 @@ public class Login extends AppCompatActivity {
         getSupportActionBar().hide();
         //-----------------------------------------------------------
         setContentView(R.layout.activity_login);
+        signupemail=findViewById(R.id.signupemail);
+        signuppassword=findViewById(R.id.signuppassword);
+        signupbtn=findViewById(R.id.signupbtn);
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=signupemail.getText().toString().trim();
+                String password=signupemail.getText().toString().trim();
+                if(email.isEmpty())
+                {
+                    signupemail.setError("Enter a Valid Email Address");
+                    signupemail.requestFocus();
+                }
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    signupemail.setError("Enter a Valid Email Address");
+                    signupemail.requestFocus();
+                }
+                if(password.isEmpty())
+                {
+                    signuppassword.setError("Enter a Password");
+                    signuppassword.requestFocus();
+                }
+                if(password.length()<6)
+                {
+                    signuppassword.setError("Minimum length of password is 6");
+                    signuppassword.requestFocus();
+                }
+
+            }
+        });
+
+
     }
 }
